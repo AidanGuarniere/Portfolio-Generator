ab01c842eb88320ccdbda096f96bd5bcf0ef57dd
aidan <aidanguarniere@gmail.com>
2020-12-07 16:31:05 -0800

    generate HTML file

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
+// requirements start 
+
+// enable file system
+const fs = require("fs");
+
+// import generatePage function from page-template.js
+const generatePage = require('./src/page-template.js');
+
+//requirements end 
+
+// create profile data array
 const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+// assign objects to the array
+const [name, github] = profileDataArgs;
 
-  console.log('================');
+// write file with file system
+fs.writeFile("./index.html", generatePage(name, github), (err) => {
+  // on fail
+  if (err) throw new Error(err);
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+  // on success
+  console.log("Portfolio complete! Check out index.html to see the output!");
+});
